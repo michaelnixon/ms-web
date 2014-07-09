ee9ba77ed6aadc6c77bf6abfa0d4d40ff59b64af
Michael Nixon <mnixon@gmail.com>
2014-07-08 22:58:28 -0700

    update ui

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,16 @@ module ApplicationHelper
     end
   end
   
+  # relies on sending type with a string that matches to a glyphicon class
+  def link_html(type, text = '')
+    base_link = raw("<span class='glyphicon glyphicon-#{type}'></span>")
+    if text.empty?
+      base_link
+    else
+      "#{base_link} #{text}"
+    end
+  end
+  
   def remove_link_html(text = '')
     base_link = raw("<span class='glyphicon glyphicon-trash'></span>")
     if text.empty?
