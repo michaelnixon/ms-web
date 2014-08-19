6e87d39aba8cb45bee0440c116a093d7d0c62aa8
Michael Nixon <mnixon@gmail.com>
2014-08-18 20:36:36 -0700

    update logos and header, along with getting jquery

--- app/assets/javascripts/custom-isotope.js
+++ app/assets/javascripts/custom-isotope.js
@@ -60,3 +60,16 @@ $(document).ready(function(){
     interval: false
   });
 });
+
+$(document).ready(function () {
+    $('.nav li a').click(function(e) {
+
+        $('.nav li').removeClass('active');
+
+        var $parent = $(this).parent();
+        if (!$parent.hasClass('active')) {
+            $parent.addClass('active');
+        }
+        e.preventDefault();
+    });
+});
